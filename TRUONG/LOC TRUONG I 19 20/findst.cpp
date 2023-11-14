c4220409eb98cf6c5b1e45725318f07f140b72db
EvilTasuki2009 <145896735+EvilTasuki2009@users.noreply.github.com>
2023-11-15 00:15:38 +0700

    Add files via upload

--- TRUONG/LOC TRUONG I 19 20/findst.cpp
+++ TRUONG/LOC TRUONG I 19 20/findst.cpp
@@ -18,11 +18,12 @@ int main(){
     freopen("findst.inp", "r", stdin);
     freopen("findst.out", "w", stdout);
 
-    string s="", s1="", sx = "";
+    string s="", s1="";
+    char ch[80];
     bool own = false;
-    long sum = 0, j = 0;
+    long sum = 0, dx = 0, maxx = 0;
     cin >> s;
-
+	cout<<s<<endl;
     for(int i = 0; i < s.size(); i++){
         own = false;
         for(int j = 0; j < s1.size(); j++){
@@ -38,12 +39,21 @@ int main(){
     cout<<sum<<endl;
 
     for(int i = 0; i < s.size(); i++){
-        for(int j = i; j < s.size(); j++){
-
+        for(int j = i+1; j < s.size(); j++){
+        	/*if(i == 19){
+        		cout<<"i: "<<i<<' '<<"j: "<<j<<' '<<"j - i: "<<j - i + 1<<' '<<s.substr(i, j-i+1)<<endl;
+			}*/
+			if(isdx(s.substr(i, j-i+1))){
+				//cout<<"i: "<<i<<' '<<"j: "<<j<<' '<<"j - i: "<<j - i + 1<<' '<<s.substr(i, j-i+1)<<endl;
+				if(maxx < j-i+1){
+					
+					maxx = j-i+1;
+				}
+			}
         }
     }
-
-
-
+    
+    cout<<maxx;
+	
     return 0;
 }
