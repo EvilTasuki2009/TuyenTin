a84cb28364fcb66977d7b4d657b42601b66ac898
EvilTasuki2009 <145896735+EvilTasuki2009@users.noreply.github.com>
2023-11-16 07:50:11 +0700

    Add files via upload

--- TRUONG/LOC TRUONG I 23 24/gameexp.cpp
+++ TRUONG/LOC TRUONG I 23 24/gameexp.cpp
@@ -5,16 +5,38 @@ int main(){
     freopen("gameexp.inp", "r", stdin);
     freopen("gameexp.out", "w", stdout);
 
-    long sum=0, n;
+    long n, channn = LONG_MAX, leln = 0, indexn = 0, indexl = 0;
     cin >> n;
     long a[n];
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0){
+            if(channn > a[i]){
+                channn = a[i];
+                indexn = i;
+            }
+        }
+        if(i % 2 != 0){
+            if(leln < a[i]){
+                leln = a[i];
+                indexl = i;
+            }
+        }
+    }
+    swap(a[indexl], a[indexn]);
+    long sum = 0;
 
     for(int i = 0; i < n; i++){
-        if()
+        if(i % 2 == 0){
+            sum += a[i];
+        }
+        else{
+            sum -= a[i];
+        }
     }
+    cout<<sum;
 
     return 0;
 }
